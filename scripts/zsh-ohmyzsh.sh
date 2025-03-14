db7886ac7d4c63339aab3642588251869c7ec48c
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-03-14 14:20:53 +0100

    Update zsh-ohmyzsh.sh to source utils from osx and install Oh-My-ZSH with zsh-autosuggestions

--- scripts/zsh-ohmyzsh.sh
+++ scripts/zsh-ohmyzsh.sh
@@ -1,6 +1,6 @@
-source './scripts/utils.sh'
+source './osx/utils.sh'
 
-# Zshell : Sets ZSH as a OS X shell
+# Zshell : Sets ZSH as a OS X shell and installs Oh-My-ZSH
 
 ZShell() {
 
@@ -8,6 +8,19 @@ ZShell() {
 	print_success 'Changing to ZSH (shell)'
 
 	chsh -s /bin/zsh
+
+	directory_backup "$HOME/.oh-my-zsh"
+
+	# Installing Oh-My-ZSH
+	cd ~
+	curl -L http://install.ohmyz.sh | sh &> /dev/null
+
+	file_exists "$HOME/.zshrc"
+	# execute "curl -L -o ~/.oh-my-zsh/custom/themes/materialshell.zsh-theme https://raw.githubusercontent.com/carloscuesta/materialshell/master/materialshell.zsh"
+
+  execute "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+
+	print_success 'Oh-My-Zsh Installed.'
 }
 
 ZShell
