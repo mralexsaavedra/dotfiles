010ae939f5f2831be27ebbd212e24da5f7d2694e
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-03-14 14:56:40 +0100

    Enhance nvm script to verify Node and NPM installation with version output

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -11,8 +11,14 @@ nvm() {
 		if cmd_exists "nvm"; then
 			print_success 'NVM has been succesfully installed!'
 
-      print_in_blue "install node"
-      execute "nvm install node"
+			print_in_blue "install node"
+      nvm install node
+
+			node --version
+      print_success "Node has been installed!"
+
+			npm --version
+			print_success "NPM has been installed!"
 		else
 			print_error 'NVM not installed.'
 		fi
